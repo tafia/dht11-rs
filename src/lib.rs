072fa592bcb051935d50ee3797ce680bfd5d77e0
Johann Tuffe <tafia973@gmail.com>
2016-12-08 11:23:14 +0800

    Remove epoll usage

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ extern crate error_chain;
 
 mod errors;
 
-use sysfs_gpio::{Pin, Direction, Edge};
+use sysfs_gpio::{Pin, Direction};
+use sysfs_gpio::Error::Unexpected;
 use std::thread;
 use std::time::{Duration, SystemTime};
 use errors::*;
@@ -25,7 +26,7 @@ impl DHT11 {
     /// Reads humidity and temperature
     pub fn read(&mut self) -> Result<Response> {
 
-        let mut times = Vec::with_capacity(41);
+        let mut times = Vec::with_capacity(40);
         self.pin.with_exported(|| {
 
             // send init signal
@@ -36,38 +37,62 @@ impl DHT11 {
             thread::sleep(Duration::new(0, 40_000));
 
             // getting sensor data
-            let start = SystemTime::now();
-            let mut poller = self.pin.get_poller()?;
             self.pin.set_direction(Direction::In)?;
-            self.pin.set_edge(Edge::FallingEdge)?;
-            while poller.poll(1)?.is_some() {
-                times.push(start.elapsed().unwrap());
+            self.next_period_ns(80_000, 80_000)?; // init response
+            for _ in 0..40 {
+                times.push(self.next_period_ns(50_000, 26_000)?); // bit data
             }
             Ok(())
+
         })?;
 
         // convert times to bytes
-        let mut intervals = times.windows(2).map(|w| w[1] - w[0]);
         let mut bytes = [0u8; 5];
-        for val in bytes.iter_mut() {
-            for i in intervals.by_ref().take(8) {
+        for (val, ch) in bytes.iter_mut().zip(times.chunks(8)) {
+            for t in ch {
                 *val <<= 1;
-                if i > Duration::new(0, 80_000) { *val |= 1; }
+                if *t > 100_000 { *val |= 1; }
             }
         }
 
         // checksum
-        // let sum: u16 = bytes.iter().take(4).map(|b| *b as u16).sum();
-        // if bytes[4] as u16 != sum & 0x00FF {
-        //     Err("Invalid checksum".into())
-        // } else {
+        let sum: u16 = bytes.iter().take(4).map(|b| *b as u16).sum();
+        if bytes[4] as u16 != sum & 0x00FF {
+            Err("Invalid checksum".into())
+        } else {
             Ok(Response {
                 h_int: bytes[0],
                 h_dec: bytes[1],
                 t_int: bytes[2],
                 t_dec: bytes[3],
             })
-        //}
+        }
+    }
+
+    /// Measures the duration in nanoseconds of a period with indicative low and high durations
+    fn next_period_ns(&self, low_ns: u32, high_ns: u32) -> ::sysfs_gpio::Result<u32> {
+        let start = SystemTime::now();
+        let mut val = 0u8;
+        let mut counter = 0u8;
+        for dt in [low_ns, high_ns].iter() {
+            // make sure we're at same value at half semi-period
+            thread::sleep(Duration::new(0, dt / 2));
+            if self.pin.get_value()? != val {
+                return Err(Unexpected(format!("Expecting {} value", val)));
+            }
+            thread::sleep(Duration::new(0, dt / 2));
+            let target = 1 - val;
+            val = self.pin.get_value()?;
+            while val != target {
+                thread::sleep(Duration::new(0, 1000));
+                counter += 1;
+                if counter == 255 { // a period cannot exceed 255 microseconds
+                    return Err(Unexpected("Timeout".to_string()));
+                }
+                val = self.pin.get_value()?;
+            }
+        }
+        Ok(start.elapsed().unwrap().subsec_nanos())
     }
 
 }
